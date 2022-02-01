6bf18391b89e3d3876ae9c0ac625e9a11ab6a168
Tagbug <tagbuged@gmail.com>
2022-02-02 02:16:30 +0800

    fix: 优化Notice接口

--- app/controller/notice.js
+++ app/controller/notice.js
@@ -1,4 +1,5 @@
 const ArticleModel = require('../models/ArticleModel.js');
+const UserModel = require('../models/UserModel.js');
 
 
 // 获取喜欢和收藏用户文章的人
@@ -7,12 +8,14 @@ const getLikeUsersArticle = async ctx => {
         const { userId } = ctx.query;
         const articles = await ArticleModel.find({ userId }).select('articleId likerList postDate');
         const like = [];
-        articles.forEach((item) => {
-            const { articleId, likerList, postDate } = item;
+        for (const article of articles) {
+            const { likerList, postDate } = article;
             for (let userId of likerList) {
-                like.push({ articleId, userId, postDate })
+                const userInfo = await UserModel.findOne({ userId });
+                const articleInfo = article;
+                like.push({ articleInfo, userInfo, postDate })
             }
-        })
+        }
         ctx.body = { status: 200, like };
     } catch (err) {
         console.log(err);
@@ -26,12 +29,14 @@ const getStarUsersArticle = async ctx => {
         const { userId } = ctx.query;
         const articles = await ArticleModel.find({ userId }).select('articleId starerList postDate');
         const star = [];
-        articles.forEach((item) => {
-            const { articleId, starerList, postDate } = item;
+        for (const article of articles) {
+            const { starerList, postDate } = article;
             for (let userId of starerList) {
-                star.push({ articleId, userId, postDate });
+                const userInfo = await UserModel.findOne({ userId });
+                const articleInfo = article;
+                star.push({ articleInfo, userInfo, postDate });
             }
-        })
+        }
         ctx.body = { status: 200, star };
     } catch (err) {
         console.log(err);
